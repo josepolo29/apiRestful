f69391e8422db2f8f841999c4cda527a189aa967
Jose Polo <joseluispolo29@gmail.com>
2021-04-15 15:45:45 -0500

    Implementando el controlador ProductController

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -15,28 +15,8 @@ class ProductController extends ApiController
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $products = Product::all();
+        return $this->showAll($products);
     }
 
     /**
@@ -47,40 +27,6 @@ class ProductController extends ApiController
      */
     public function show(Product $product)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
-    {
-        //
+        return $this->showOne($product);
     }
 }

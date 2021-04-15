ac7665bb5bfc0434d54578265e73f600e45b1fc7
Jose Polo <joseluispolo29@gmail.com>
2021-04-15 14:26:01 -0500

    Resolviendo la Inyección de Seller Usando 'Global Scopes'

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -2,8 +2,15 @@
 
 namespace App\Models;
 
+use App\Scopes\SellerScope;
+
 class Seller extends User
 {
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new SellerScope);
+    }
 
     public function products(){
         return $this->hasMany(Product::class);

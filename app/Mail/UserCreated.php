e60f1c780ffb222e9591f8b4559c06c17783936b
Jose Polo <joseluispolo29@gmail.com>
2021-04-17 17:54:50 -0500

    Implementando el Mailable para Usuarios Creados

--- app/Mail/UserCreated.php
+++ app/Mail/UserCreated.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -11,14 +12,16 @@ class UserCreated extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $user;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -28,6 +31,6 @@ class UserCreated extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.welcome');
     }
 }

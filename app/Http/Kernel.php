e70cddadf9d3ba0c554866dda7517ad83abc1a9f
Jose Polo <joseluispolo29@gmail.com>
2021-04-18 10:52:18 -0500

    Limitando las peticiones solicitadas a la API... throttle:60,1,api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'signature:X-Application-Name',
-            'throttle:api',
+            'throttle:60,1,api', //Limitar las peticiones //METODO handler
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];

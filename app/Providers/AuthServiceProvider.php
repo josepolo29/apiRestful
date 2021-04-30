b2f8a5069d2fd35af50baee8505cb559250e4e46
Jose Polo <joseluispolo29@gmail.com>
2021-04-30 13:57:12 -0500

    Registro de algunos Scopes Requeridos para la API

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -37,6 +37,15 @@ class AuthServiceProvider extends ServiceProvider
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
         
         Passport::enableImplicitGrant();
+
+        Passport::tokensCan([
+            'purchase-product' => 'Crear transacciones para comprar productos determinados',
+            'manage-products' => 'Crear, ver, actualizar y eliminar productos',
+            'manage-account' => 'Obtener la información de la cuenta, nombre, email, estado (sin contraseña), modificar
+                                datos como email, nombre y contraseña. No puede eliminar la cuenta',
+            'read-general' => 'Obtener información general, categorías donde se compra y se vende, productos vendidos
+                                o comprados, transacciones, compras y ventas',
+        ]);
         //
     }
 }
